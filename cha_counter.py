3f84b79eee48ad9a0973b502d6e55e337317e1df
Deividas Januska <januska.deividas@gmail.com>
2020-12-08 15:18:40 +0200

    final commit

--- cha_counter.py
+++ cha_counter.py
@@ -6,45 +6,75 @@ from glob import glob
 
 
 def inGroup(group, name):
-    for per in group:
-        if per.name == name:
+    for person in group:
+        if person.name == name:
             return True
     return False
 
 
 def findIndex(group, name):
-    for num, person in enumerate(group):
+    for idx, person in enumerate(group):
         if person.name == name:
-            return num
-    return -1
+            return idx
+    return None
 
 
 class Person():
     def __init__(self, name):
         self.name = name
-        # number of given chas to each participant will be stored in this dictionary
+        # Number of given chas to each participant will be stored in this dictionary
         self.gaveCha = {}
-        # number of received chas from each participant will be stored in this dictionary
-        self.receiveCha = {}
+        # Number of received chas from each participant will be stored in this dictionary
+        self.receivedCha = {}
         self.numMessages = 0
         self.numReceivedCha = 0
         self.numGivenCha = 0
 
-    def return_data(self):
+    def return_basic_data(self):
         return "{}\n{}\n{}\n{}\n".format(self.name, self.numMessages,  self.numGivenCha, self.numReceivedCha)
 
     def return_csv(self):
-        return "{},{},{},{}\n".format(self.name, self.numMessages, self.numReceivedCha, self.numGivenCha)
+        return_str = "{},{},{},{}".format(
+            self.name, self.numMessages, self.numReceivedCha, self.numGivenCha)
+
+        return_str += ",|"
+
+        for item in sorted(self.receivedCha.items()):
+            return_str += ",{}".format(item[1])
+
+        return_str += ",|"
+
+        for item in sorted(self.gaveCha.items()):
+            return_str += ",{}".format(item[1])
+
+        return_str += "\n"
+
+        return return_str
+
+    def print_data(self):
+        print("Name: {}\nNumber of sent messages: {}\nNum of received chas: {}\nNumber of given chas:{}\n".format(
+            self.name, self.numMessages, self.numReceivedCha, self.numGivenCha))
+
+        print("Received chas from participants:")
+        for item in sorted(self.receivedCha.items()):
+            print("{}: {}".format(item[0], item[1]))
+
+        print("\nGiven chas to participants:")
+        for item in sorted(self.gaveCha.items()):
+            print("{}: {}".format(item[0], item[1]))
+        print("\n------------------------------------------")
 
 
 def main():
-    # check if path argument is provided
+    # Check if path argument is provided
     if len(sys.argv) < 2:
         sys.exit(
             'Too few arguments provided.\nCommand exmaple:\n\tpython3 cha_counter.py <relative path to the specific chat folder>')
 
-    # check if path exists
-    if not path.exists(sys.argv[1]):
+    file_path = sys.argv[1]
+
+    # Check if path exists
+    if not path.exists(file_path):
         sys.exit('Provided relative path does not exist.')
 
     pattern = path.join(
@@ -53,42 +83,77 @@ def main():
     group = []
 
     for file_name in glob(pattern):
-        # iterate through each *.json file
+        # Iterate through each *.json file
         with open(file_name) as f:
             data = json.load(f)
-            # check if there are any new participants in the group
+            # Check if there are any new participants in the group
             for person in data["participants"]:
                 if not inGroup(group, person["name"]):
-                    # new participant detected
+                    # New participant detected
                     group.append(Person(person["name"]))
 
+            # Add new participants to Person.gaveCha/Person.receivedCha for each Person in group.
             for person in group:
                 for per in data["participants"]:
                     if per["name"] not in person.gaveCha:
                         person.gaveCha[per["name"]] = 0
-                        person.receiveCha[per["name"]] = 0
+                        person.receivedCha[per["name"]] = 0
 
+            # Iterate through each message in *.json file
             for message in data["messages"]:
-                # increment the sent messages counter
+                # Increment the sent messages counter
                 senderIndex = findIndex(group, message["sender_name"])
+
+                if senderIndex == None:
+                    continue
+
                 group[senderIndex].numMessages += 1
 
-                # search for cha reactions
+                # Search for cha reactions
                 if "reactions" in message:
                     for reaction in message["reactions"]:
+                        # Unicode for cha reaction
                         if reaction["reaction"] == u"\u00f0\u009f\u0098\u0086":
+                            giverName = reaction["actor"]
                             giverIndex = findIndex(group, reaction["actor"])
-                            giverName = group[giverIndex].name
-                            group[senderIndex].numReceivedCha += 1
-                            group[senderIndex].receiveCha[giverName] += 1
 
+                            if giverIndex == None:
+                                continue
+
+                            # Message sender receives a Cha react from giverName
+                            group[senderIndex].numReceivedCha += 1
+                            group[senderIndex].receivedCha[giverName] += 1
+
+                            # Actor gives a Cha react to a sender
                             group[giverIndex].numGivenCha += 1
                             group[giverIndex].gaveCha[message["sender_name"]] += 1
-            file = codecs.open(
-                "stats.csv", "w", "iso-8859-1")
-            for person in group:
-                file.write(person.return_csv())
-            file.close()
+
+    # Find group name
+    last_slash_index = file_path.rfind("/") + 1
+    group_name = file_path[last_slash_index:]
+
+    file = codecs.open(
+        "stats_{}.csv".format(group_name), "w", "iso-8859-1")
+
+    # Constructing and writing header
+    header_line = "Participant name,Number of sent messages,Number of received chas,Number of sent chas,|Received chas from"
+    for item in sorted(group[0].receivedCha.items()):
+        header_line += ",{}".format(item[0])
+    header_line += ",|Sent chas to"
+    for item in sorted(group[0].gaveCha.items()):
+        header_line += ",{}".format(item[0])
+    header_line += "\n"
+    file.write(header_line)
+
+    # Sort participant list by Person.name
+    group.sort(key=lambda person: person.name)
+
+    # Writing data
+    for person in group:
+        file.write(person.return_csv())
+    file.close()
+
+    print("Stats successfully calculated and saved to:\n\tstats_{}.csv".format(group_name))
 
 
 if __name__ == "__main__":
---
+++ readme.md
@@ -0,0 +1,21 @@
+# "Cha Cha" reaction counting script
+
+The script analyzes basic stats of a Facebook chat.
+
+It gathers the following information:
+
+* The number of messages each participant has sent;
+* The number of received Cha Cha reactions each participant gave/received;
+
+The script also figures out how many times each participant reacted to a particular participant's message in the group chat.
+
+Calculated data is written to a *.csv file.
+
+## Usage
+
+1. Download Facebook Messenger data in JSON format:
+https://www.facebook.com/dyi/?referrer=yfi_settings
+2. Run the script specifying a relative path to the target chat folder: 
+`python3 cha_counter.py <relative path to the folder>`
+Example:
+`python3 cha_counter.py data/facebook-facebookuser1/messages/inbox/groupname_xyz`
